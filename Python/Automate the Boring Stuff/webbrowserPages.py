35319d0b0e70fc18922e82c75445147642b7f5f4
Melina Rentzi <MelinaRentzi@users.noreply.github.com>
2017-10-08 20:30:43 +0300

    Delete webbrowserPages.py

--- Python/Automate the Boring Stuff/webbrowserPages.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-
-webbrowser.open('https://www.reddit.com/')
-webbrowser.open('https://qtfy.eu/', new=2) #new tab
-webbrowser.open('http://discretize.eu/#fractals', new=2) #new tab
